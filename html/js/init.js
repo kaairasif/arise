edf83af026d262a8bb992cfdeb9bdf0ce1ae01f7
asif <asifcse22@gmail.com>
2017-09-12 02:16:42 +0600

    Html update

--- html/js/init.js
+++ html/js/init.js
@@ -6,8 +6,18 @@
   "use strict";
 
 /* =
-  PRE LOADER                     
-  =================================== */
+PRE LOADER                     
+=================================== */
+
+function setDivHeight() {
+    //height of browser viewport
+    var viewportHeight = $(window).height();
+    $('.home').height(viewportHeight); // OR viewportHeight - 180 as appropriate
+}
+
+// = onload and onresize
+$(window).on("load resize", setDivHeight);
+
 
 
 
